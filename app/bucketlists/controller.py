fa44a49910010412bbfc4bbfdee36937a7ec9903
Zacharia <zacmwangi94@gmail.com>
2017-07-13 19:02:16 +0300

    [FEATURE]Add create bucket list method to controller

---
+++ app/bucketlists/controller.py
@@ -0,0 +1,10 @@
+from app import db
+from models import BucketList
+
+
+def create_bucket_list(data):
+    title = data.get('title')
+    description = data.get('description')
+    bucketlist = BucketList(title=title,description=description)
+    db.session.add(bucketlist)
+    db.session.commit()
