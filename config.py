0aa33cd3b69b213a6e8e59246a5e158142827c02
Zacharia <zacmwangi94@gmail.com>
2017-07-21 17:40:42 +0300

    [CONFIG]Modify test config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
-
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     ERROR_404_HELP = False
+    SECRET_KEY = 'nqqijvwyv+8@kwag_9k^&2gnvw40qf34^=l$s6ph#3vnug4f)'
     pass
 
 
@@ -13,16 +13,14 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     pass
 
-
 class TestingConfig(Config):
-    SECRET_KEY = 'nqqijvwyv+8@kwag_9k^&2gnvw40qf34^=l$s6ph#3vnug4f)'
 
     DEBUG = False
     TESTING = True
     WTF_CSRF_ENABLED =False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = ('sqlite:///' +
-                                os.path.join('./', 'bucket_list.db'))
+                                os.path.join(basedir, 'bucket_list.db'))
 
 app_config = {
     'development': DevelopmentConfig,
