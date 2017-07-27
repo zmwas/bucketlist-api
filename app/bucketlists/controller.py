783fcc4f4b7fcc433271c8c9b91827be60122e3f
Zacharia <zacmwangi94@gmail.com>
2017-07-27 14:06:04 +0300

    [REFACTOR]Add utility function to get a single bucket list item

--- app/bucketlists/controller.py
+++ app/bucketlists/controller.py
@@ -20,8 +20,8 @@ def get_single_bucketlist(id):
     return bucketlist
 
 def delete_bucket_list(id):
-    bucketlist = BucketList.query.get(id)
-    if bucketlist == None:
+    bucketlist = get_single_bucketlist(id)
+    if bucketlist == "Bucketlist doesn't exist":
         return "Bucketlist doesn't exist"
     db.session.delete(bucketlist)
     db.session.commit()
@@ -29,12 +29,15 @@ def delete_bucket_list(id):
 def update_bucket_list(id,data):
     title = data.get('title')
     description = data.get('description')
-    bucketlist = BucketList.query.get(id)
+    bucketlist = get_single_bucketlist(id)
+    if bucketlist == "Bucketlist doesn't exist":
+        return "Bucketlist doesn't exist"
     bucketlist.title = title
     bucketlist.description = description
     db.session.add(bucketlist)
     db.session.commit()
     return bucketlist
+
 
 def create_bucket_list_item(data):
     name = data.get('name')
@@ -43,4 +46,26 @@ def create_bucket_list_item(data):
     bucket_list = BucketList.query.filter_by(id=bucketlist_id).first()
     bucket_list.bucketlistitems.append(item)
     db.session.add(item)
+    db.session.commit()
+
+def get_single_bucketlist_item(id,item_id):
+    item = BucketListItem.query.filter_by(id=item_id,bucketlist_id=id).first()
+    if item == None:
+        return "Item doesn't exist"
+    return item
+
+
+def update_bucket_list_item(id,item_id,data):
+    name = data.get('name')
+    item = get_single_bucketlist_item(id,item_id)
+    if item == "Item doesn't exist":
+        return "Item doesn't exist"
+    item.name = name
+    db.session.add(item)
+    db.session.commit()
+def delete_bucket_list_items(id,item_id):
+    item = get_single_bucketlist_item(id,item_id)
+    if item == "Item doesn't exist":
+        return "Item doesn't exist"
+    db.session.delete(item)
     db.session.commit()
