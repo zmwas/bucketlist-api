10b1aac8f934752cacb1b4d3f7fed599bd75dd0b
Zacharia <zacmwangi94@gmail.com>
2017-07-27 10:06:11 +0300

    [FEATURE]Create table for bucketlist items

--- app/bucketlists/models.py
+++ app/bucketlists/models.py
@@ -8,3 +8,11 @@ class BucketList(db.Model):
     id  = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(25),unique=True)
     description = db.Column(db.String(250),unique=False)
+    bucketlistitems = db.relationship('BucketListItem',backref='bucketlist',lazy='dynamic')
+
+class BucketListItem(db.Model):
+
+    __tablename__ = "bucketlistitems"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25),unique=True)
+    bucketlist_id = db.Column(db.Integer,db.ForeignKey('bucketlists.id'))
