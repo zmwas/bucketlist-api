66c6d79ec0bd895b8680ee0c3fe432f5259fd58a
Zacharia <zacmwangi94@gmail.com>
2017-08-05 15:35:28 +0300

    [FIX]Modify method for checking for password

--- app/users/views.py
+++ app/users/views.py
@@ -49,6 +49,6 @@ class RegisterUserResource(Resource):
 class LoginUserResource(Resource):
     @auth.login_required
     def post(self):
-        print(g.user)
+        print(g.user.__dict__)
         token = g.user.generate_auth_token()
         return jsonify({'Authorization': token.decode('ascii')})
