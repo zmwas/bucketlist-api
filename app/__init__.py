bdadcdb77d4cba10ea80ce09a81df09755f4c784
Zacharia <zacmwangi94@gmail.com>
2017-08-18 15:16:48 +0300

    [FIX]Add cors support

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 from .utils import api
 from config import app_config
 
@@ -16,6 +16,7 @@ from users.views import namespace as auth_namespace
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
+    CORS(app)
     db.init_app(app)
     api.init_app(app)
     api.add_namespace(bucketlist_namespace)
